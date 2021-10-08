f2e9e711cb35a8164535d881b8d886fb315a4219
EliCarrizo <elikarry22c@gmail.com>
2021-10-08 01:25:54 -0300

    change index.

---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,10 @@
+/*Variables y Operadores
+Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.*/
+
+var age = 31
+
+if(age > 21) {
+    console.log( "is ok")
+
+}
+
